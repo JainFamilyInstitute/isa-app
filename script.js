e0487e201329a243b1c9eb8fb0dbd43730fbe403
3milychu <3milychu@gmail.com>
2018-11-05 13:03:55 -0500

    added data structure planning notebook for vis and test filtering logic

---
+++ script.js
@@ -0,0 +1,60 @@
+// dimensions
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// range
+var x = d3.scaleBand()
+  .range([0, width])
+  .padding(0.1);
+
+var y = d3.scaleLinear()
+  .range([height, 0]);
+
+// define the line
+var valueline = d3.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.close); });
+
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("#income")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+d3.csv("https://raw.githubusercontent.com/JainFamilyInstitute/isa-app/master/data/data_vis.csv?token=AXiiVXcAwXZjLK4-3tiyxKwj8yaVMVDmks5b6b8NwA%3D%3D", function(error, data) {
+  if (error) throw error;
+
+  // format the data
+  data.forEach(function(d) {
+  });
+
+  data_filtered = data.map(({ quantile,amount,risk,type,variable, ...item }) => item);
+
+  console.log(data_filtered);
+
+  // // Scale the range of the data
+  // x.domain(d3.extent(data, function(d) { return d.date; }));
+  // y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+  // // Add the valueline path.
+  // svg.append("path")
+  //     .data([data])
+  //     .attr("class", "line")
+  //     .attr("d", valueline);
+
+  // // Add the X Axis
+  // svg.append("g")
+  //     .attr("transform", "translate(0," + height + ")")
+  //     .call(d3.axisBottom(x));
+
+  // // Add the Y Axis
+  // svg.append("g")
+  //     .call(d3.axisLeft(y));
+
+});
