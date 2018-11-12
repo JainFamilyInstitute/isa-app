fe796c4dd0e3773cf60a443b47d90b3b62030852
3milychu <3milychu@gmail.com>
2018-11-12 14:28:46 -0500

    plug in data to interface

--- script.js
+++ script.js
@@ -1,33 +1,132 @@
 // variables we'll be filtering by
+var grad;
 var quantile;
-// what about income level?
 var amount;
 var type;
 var risk;
 
-w = window.innerWidth;
-h = window.innerHeight;
+function getInnerWidth(elem) {
+    return parseFloat(window.getComputedStyle(elem).width);
+}
+
+function getInnerHeight(elem) {
+    return parseFloat(window.getComputedStyle(elem).height);
+}
+
+var chart = document.querySelector('#chart1');
+
+w = getInnerWidth(chart);
+h = getInnerHeight(chart);
 
 // <-- Make Selection -->
 // Possible values --
+// grad: [1 0]
 // quantile: [1 2 3 4]
 // variable: ['Income' 'Payments' 'Consumption' 'Utility']
 // amount: [ 5000 30000 70000]
-// type: ['Loan' 'ISA']
+// type: ['Loan' 'ISA' 'Free']
 // risk: [1 2 3 4]
 
 // now let's pretend we're making a selection - this will be dynamically stored based on user input later
-quantile = 1;
-amount=5000;
-type = 'Loan';
-risk = 2;
+var grad_default = document.querySelector('input[name="grad"][value="1"]').checked = true;
+  var quantile_default = document.querySelector('input[name="quantile"][value="1"]').checked = true;
+  var amount_default = document.querySelector('input[name="amount"][value="70000"]').checked = true;
+  var risk_default = document.querySelector('input[name="risk"][value="1"]').checked = true;
 
-d3.csv("https://raw.githubusercontent.com/JainFamilyInstitute/isa-app/master/data/data_vis.csv?token=AXiiVXcAwXZjLK4-3tiyxKwj8yaVMVDmks5b6b8NwA%3D%3D", function(error, data) {
-//   if (error) throw error;
-getIncome(data);
-getPayments(data);
+var grad_checked = document.querySelectorAll('input[name="grad"]:checked');
+    var quantile_checked = document.querySelectorAll('input[name="quantile"]:checked');
+    var amount_checked = document.querySelectorAll('input[name="amount"]:checked');
+    var risk_checked = document.querySelectorAll('input[name="risk"]:checked');
 
-});
+for(i=0;i<grad_checked.length;i++){
+  grad=grad_checked[i].value;
+  console.log(grad)
+}
+for(i=0;i<quantile_checked.length;i++){
+  quantile=quantile_checked[i].value;
+}
+for(i=0;i<amount_checked.length;i++){
+  amount=amount_checked[i].value;
+}
+for(i=0;i<risk_checked.length;i++){
+  risk=risk_checked[i].value;
+}
+type="ISA";
+
+var radios = document.querySelectorAll('input');
+var alerts = document.querySelectorAll('.alert');
+var message1 = document.querySelector('#message1');
+var message2 = document.querySelector('#message2');
+var svg1 = document.querySelector('#one');
+var svg2 = document.querySelector('#two');
+
+status();
+
+
+for (i=0;i<radios.length;i++){
+  radios[i].onchange=function() {
+    status();
+    console.log(grad + quantile + amount + type + risk);
+    update();
+  }
+}
+
+function status() {
+    grad_checked = document.querySelectorAll('input[name="grad"]:checked');
+    quantile_checked = document.querySelectorAll('input[name="quantile"]:checked');
+    amount_checked = document.querySelectorAll('input[name="amount"]:checked');
+    risk_checked = document.querySelectorAll('input[name="risk"]:checked');
+    console.log(grad_checked.length + "," + quantile_checked.length + "," + amount_checked.length + "," + risk_checked.length)
+    if(grad_checked.length == 0 | quantile_checked.length == 0 & amount_checked.length == 1 & risk_checked ==1){
+      alerts[0].style.display="inline-block";
+      alerts[0].style.width="100%";
+      alerts[1].style.display="inline-block";
+      alerts[1].style.width="100%";
+      svg1.style.display="none";
+      svg2.style.display="none";
+      message1.innerHTML="Please enter your demographics"
+      message2.innerHTML="Please enter your demographics"
+    }
+    if (grad_checked.length == 1 & quantile_checked.length == 1 & amount_checked.length == 1 & risk_checked.length == 1){
+      alerts[0].style.display="none";
+      alerts[1].style.display="none";
+      svg1.style.display="inline-block";
+      svg2.style.display="inline-block";
+      for(i=0;i<grad_checked.length;i++){
+        grad=grad_checked[i].value;
+      }
+      for(i=0;i<quantile_checked.length;i++){
+        quantile=quantile_checked[i].value;
+      }
+      for(i=0;i<amount_checked.length;i++){
+        amount=amount_checked[i].value;
+      }
+      for(i=0;i<risk_checked.length;i++){
+        risk=risk_checked[i].value;
+      }
+    }
+}
+
+
+function loadData() {
+  d3.csv("https://raw.githubusercontent.com/JainFamilyInstitute/isa-app/master/data/data_vis.csv?token=AXiiVQOBnY3R6QBfyMv1QE-0q7HRLS_Bks5b8vs3wA%3D%3D", function(error, data) {
+  //   if (error) throw error;
+  getIncome(data);
+  getPayments(data);
+
+  });
+}
+
+loadData();
+
+function update() {
+  d3.csv("https://raw.githubusercontent.com/JainFamilyInstitute/isa-app/master/data/data_vis.csv?token=AXiiVQOBnY3R6QBfyMv1QE-0q7HRLS_Bks5b8vs3wA%3D%3D", function(error, data) {
+  //   if (error) throw error;
+  updateIncome(data);
+  updatePayments(data);
+
+  });
+}
 
 
 // wrap modular activities into functions
@@ -36,9 +135,9 @@ function getIncome(data) {
 variable= 'Income';
 
 // chart stuff
- margin = {top: h/4, right: w/4, bottom: h/4, left: w/4},
-    width = window.innerWidth - margin.left - margin.right,
-    height = window.innerHeight - margin.top - margin.bottom;
+ margin = {top: h/10, right: w/10, bottom: h/10, left: w/10},
+    width = w - margin.left - margin.right,
+    height = h - margin.top - margin.bottom;
 
 x = d3.scaleLinear()
   .rangeRound([0, width]);
@@ -50,16 +149,13 @@ valueline = d3.line()
     .x(function(d) { return x(d.key); })
     .y(function(d) { return y(d.value); });
 
-var svg1 = d3.select("#inc_payments")
+var svg1 = d3.select("#one")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-// data
-// d3.csv("https://raw.githubusercontent.com/JainFamilyInstitute/isa-app/master/data/data_vis.csv?token=AXiiVXcAwXZjLK4-3tiyxKwj8yaVMVDmks5b6b8NwA%3D%3D", function(error, data) {
-//   if (error) throw error;
 
   // filter by selection
   data = data.filter(function(d) { 
@@ -113,8 +209,6 @@ var svg1 = d3.select("#inc_payments")
       .tickSize(-width)
       );
 
-// });
-
 }
 
 
@@ -123,9 +217,9 @@ function getPayments(data) {
 variable= 'Payments';
 
 // chart stuff
-margin = {top: h/4, right: w/4, bottom: h/4, left: w/4},
-    width = window.innerWidth - margin.left - margin.right,
-    height = window.innerHeight - margin.top - margin.bottom;
+margin = {top: h/10, right: w/10, bottom: h/10, left: w/10},
+    width = w - margin.left - margin.right,
+    height = h - margin.top - margin.bottom;
 
 x = d3.scaleLinear()
   .rangeRound([0, width]);
@@ -137,16 +231,12 @@ valueline = d3.line()
     .x(function(d) { return x(d.key); })
     .y(function(d) { return y(d.value); });
 
-var svg2 = d3.select("#consumption")
+var svg2 = d3.select("#two")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
-
-// data
-// d3.csv("https://raw.githubusercontent.com/JainFamilyInstitute/isa-app/master/data/data_vis.csv?token=AXiiVXcAwXZjLK4-3tiyxKwj8yaVMVDmks5b6b8NwA%3D%3D", function(error, data) {
-//   if (error) throw error;
 
   // filter by selection
   data = data.filter(function(d) { 
@@ -193,6 +283,276 @@ var svg2 = d3.select("#consumption")
   svg2.append("g")
       .call(d3.axisLeft(y));
 
-// });
+}
+
+// update data functions
+
+// chart stuff
+margin = {top: h/10, right: w/10, bottom: h/10, left: w/10},
+    width = w - margin.left - margin.right,
+    height = h - margin.top - margin.bottom;
+
+x = d3.scaleLinear()
+  .rangeRound([0, width]);
+
+y = d3.scaleLinear()
+  .rangeRound([height,0]);
+
+valueline = d3.line()
+    .x(function(d) { return x(d.key); })
+    .y(function(d) { return y(d.value); });
+
+
+function getIncome(data) {
+
+variable= 'Income';
+
+var svg1 = d3.select("#one")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+
+  // filter by selection
+  data = data.filter(function(d) { 
+          return (d.quantile == quantile) &
+          (d.variable == variable) &
+          (d.amount == amount) &
+          (d.type == type) &
+          (d.risk == risk) }); 
+
+  // create visualizable array with only ages and amounts for selected series
+  data_filtered = data.map(({ quantile,amount,risk,type,variable, ...item }) => item);
+  data_vis = data_filtered[0];
+  console.log(data_vis);
+
+  result = [];
+  for(i=22;i<101;i++){
+    key = i;
+    value =parseFloat(data_vis[i]);
+    result.push({
+            key: key,
+            value: value
+        });
+  }
+
+  // console.log(data_vis);
+  console.log(result);
+
+  // Scale the range of the data
+  x.domain([d3.min(result, function(d) { return d.key; }), d3.max(result, function(d) { return d.key; })]);
+  y.domain([0, d3.max(result, function(d) { return d.value; })]);
+
+  // Add the valueline path.
+  svg1.append("path")
+      .data([result])
+      .attr("class", "line")
+      .attr("id", "line1")
+      .attr("d", valueline);
+
+  // Add the X Axis
+  svg1.append("g")
+      .attr("id","x-axis1")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // Add the Y Axis
+    function y_grid_lines() {
+    return d3.axisLeft(y)
+  }
+
+  svg1.append("g")
+    .attr("class", "grid")
+    .attr("id","y-axis1")
+    .call(y_grid_lines()
+      .tickSize(-width)
+      );
 
 }
+
+
+function getPayments(data) {
+
+variable= 'Payments';
+
+var svg2 = d3.select("#two")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+  // filter by selection
+  data = data.filter(function(d) { 
+          return (d.quantile == quantile) &
+          (d.variable == variable) &
+          (d.amount == amount) &
+          (d.type == type) &
+          (d.risk == risk) }); 
+
+  // create visualizable array with only ages and amounts for selected series
+  data_filtered = data.map(({ quantile,amount,risk,type,variable, ...item }) => item);
+  data_vis = data_filtered[0];
+  console.log(data_vis);
+
+  result = [];
+  for(i=22;i<101;i++){
+    key = i;
+    value =parseFloat(data_vis[i]);
+    result.push({
+            key: key,
+            value: value
+        });
+  }
+
+  // console.log(data_vis);
+  console.log(result);
+
+  // Scale the range of the data
+  x.domain([d3.min(result, function(d) { return d.key; }), d3.max(result, function(d) { return d.key; })]);
+  y.domain([0, d3.max(result, function(d) { return d.value; })]);
+
+  // Add the valueline path.
+  svg2.append("path")
+      .data([result])
+      .attr("class", "line")
+      .attr("id", "line2")
+      .attr("d", valueline);
+
+  // Add the X Axis
+  svg2.append("g")
+      .attr("id","x-axis2")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // Add the Y Axis
+  svg2.append("g")
+      .attr("id","y-axis2")
+      .call(d3.axisLeft(y));
+
+}
+
+// update data functions
+
+function updateIncome(data) {
+
+variable= 'Income';
+
+var svg1 = d3.select("#one").transition();
+
+
+  // filter by selection
+  data = data.filter(function(d) { 
+          return (d.quantile == quantile) &
+          (d.variable == variable) &
+          (d.amount == amount) &
+          (d.type == type) &
+          (d.risk == risk) }); 
+
+  // create visualizable array with only ages and amounts for selected series
+  data_filtered = data.map(({ quantile,amount,risk,type,variable, ...item }) => item);
+  data_vis = data_filtered[0];
+  console.log(data_vis);
+
+  result = [];
+  for(i=22;i<101;i++){
+    key = i;
+    value =parseFloat(data_vis[i]);
+    result.push({
+            key: key,
+            value: value
+        });
+  }
+
+  // console.log(data_vis);
+  console.log(result);
+
+  // Scale the range of the data
+  x.domain([d3.min(result, function(d) { return d.key; }), d3.max(result, function(d) { return d.key; })]);
+  y.domain([0, d3.max(result, function(d) { return d.value; })]);
+
+  // Add the valueline path.
+  svg1.select("#line1")
+      .duration(750)
+      .attr("d", valueline(result));
+
+  // Add the X Axis
+  svg1.select("#x-axis1")
+      .duration(750)
+      .call(d3.axisBottom(x));
+
+  // Add the Y Axis
+    function y_grid_lines() {
+    return d3.axisLeft(y)
+  }
+
+  svg1.select("#y-axis1")
+    .attr("class", "grid")
+    .call(y_grid_lines()
+      .tickSize(-width)
+      );
+
+}
+
+
+function updatePayments(data) {
+
+variable= 'Payments';
+
+var svg2 = d3.select("#two").transition();
+
+  // filter by selection
+  data = data.filter(function(d) { 
+          return (d.quantile == quantile) &
+          (d.variable == variable) &
+          (d.amount == amount) &
+          (d.type == type) &
+          (d.risk == risk) }); 
+
+  // create visualizable array with only ages and amounts for selected series
+  data_filtered = data.map(({ quantile,amount,risk,type,variable, ...item }) => item);
+  data_vis = data_filtered[0];
+  console.log(data_vis);
+
+  result = [];
+  for(i=22;i<101;i++){
+    key = i;
+    value =parseFloat(data_vis[i]);
+    result.push({
+            key: key,
+            value: value
+        });
+  }
+
+  // console.log(data_vis);
+  console.log(result);
+
+  // Scale the range of the data
+  x.domain([d3.min(result, function(d) { return d.key; }), d3.max(result, function(d) { return d.key; })]);
+  y.domain([0, d3.max(result, function(d) { return d.value; })]);
+
+  svg2.select("#line2")
+      .duration(750)
+      .attr("d", valueline(result));
+
+  // Add the X Axis
+  svg2.select("#x-axis1")
+      .duration(750)
+      .call(d3.axisBottom(x));
+
+  // Add the Y Axis
+    function y_grid_lines() {
+    return d3.axisLeft(y)
+  }
+
+  svg2.select("#y-axis1")
+    .attr("class", "grid")
+    .call(y_grid_lines()
+      .tickSize(-width)
+      );
+
+}
+
+
